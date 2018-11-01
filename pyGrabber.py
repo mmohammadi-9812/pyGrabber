bddd7cf5f028d6867a14a49fe27d339c8bef53a2
mohammad <m.mohammadi9812@gmail.com>
2018-11-01 10:09:09 +0330

    linted code and formatted it better

--- pyGrabber.py
+++ pyGrabber.py
@@ -1,11 +1,11 @@
-### Web page Downloader ###
+# Web page Downloader
 
 import sys
 import urllib.request
-from urllib.parse import urlparse
 import urllib.error
 import re
 import os
+import argparse
 
 timeout_time = 2
 search_level = 1
@@ -14,23 +14,26 @@ file_format = ""
 files_to_download = set()
 save_directory = 'pyGrabber'
 
-def is_sutable_for_download(file , format):
+
+def is_sutable_for_download(file, format):
     if file.endswith(format):
         return True
     return False
 
 
-def get_sutable_links(the_set , format):
+def get_sutable_links(the_set, format):
     return set(filter(lambda x: is_sutable_for_download(x), the_set))
 
 
-def find_links_of(url , search_level):
+def find_links_of(url, search_level):
     print('\t' * search_level * (search_level-1) / 2)
     print(url)
     try:
-        links = urllib.request.urlopen(url , timeout=timeout_time)
-        links = links.read().decode('utf-8' , 'ignore')
-        links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', links)
+        links = urllib.request.urlopen(url, timeout=timeout_time)
+        links = links.read().decode('utf-8', 'ignore')
+        link_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|\
+                        (?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        links = re.findall(link_pattern, links)
         links = set(links)
         return links
     except urllib.error.URLError:
@@ -47,10 +50,13 @@ def find_links_of(url , search_level):
         print("The read operation timed out")
         return set()
 
+
 def is_url(__str__):
-    if re.match('https?://(?:www)?(?:[\w-]{2,255}(?:\.\w{2,6}){1,2})(?:/[\w&%?#-]{1,300})?',__str__):
+    if re.match(r'https?://(?:www)?(?:[\w-]{2,255}(?:\.\w{2,6}){1,2})\
+                  (?:/[\w&%?#-]{1,300})?', __str__):
         return True
     return False
+
 
 def url_file_size(url):
     try:
@@ -61,10 +67,12 @@ def url_file_size(url):
         url_size_in_bytes = int(url_size_in_bytes)
         return url_size_in_bytes
     except ValueError as e:
-        print("Cannot convert file size into int, there should be a problem in headers")
+        print("Cannot convert file size into int, there should be a \
+               problem in headers")
         print(f"got this error: {e}")
     except TimeoutError as e:
         print(f"request timed out\n{e}")
+
 
 def partial_download(url, step=100):
     total_size = url_file_size(url)
@@ -81,7 +89,8 @@ def partial_download(url, step=100):
         current_size += step
     return response
 
-def downloading(all , reqs , save_dir ):# timeout_time):
+
+def downloading(all, reqs, save_dir):
     if len(all) == 0 or len(reqs) == 0:
         print('nothing found')
         sys.exit(0)
@@ -89,60 +98,69 @@ def downloading(all , reqs , save_dir ):# timeout_time):
     print(all)
     print('downloading:')
     print(reqs)
-    print("total of " ,  end = '')
-    print(len(reqs) , end = " ")
+    print("total of ", end='')
+    print(len(reqs), end='')
     if input("files will be downloaded start download (y/n) ?") == 'y':
         for x in reqs:
-            print("downloading : " , end = '')
+            print("downloading : ", end='')
             print(x)
-            print('\t as : ' , end = '')
+            print('\tas : ', end='')
             print(x[x.rfind("/")+1:] + '\n')
             try:
-                urllib.request.urlretrieve(x , save_dir + '/' + x[x.rfind("/")+1:] )#, timeout=timeout_time)
-            except:
-                print('Download Failed')
+                urllib.request.urlretrieve(x, save_dir + '/' +
+                                           x[x.rfind("/")+1:])
+            except urllib.error as e:
+                print('Download Failed' + str(e))
+
+
+def handle_arguments():
+    parser = argparse.ArgumentParser()
+
 
 def help():
     """
     function to help users how to work with program
     """
     print("grabbs files from a specified web page with specified format")
-    print("usage: pyGrabber <website_url> <-f file_format> [-h] [-d save directory] [-l search level]")
+    print("usage: pyGrabber <website_url> <-f file_format> [-h] [-d save\
+            directory] [-l search level]")
     print("\toptions:")
     print("\t\t-f format\tsearch the pages for files with this format")
     print("\t\t-d directory\tsaves the downloade files into this directory")
     print("\t\t-l level\tint number to define deapth of search default is 0")
 
+
 if __name__ == '__main__':
-    for x in range(0,len(sys.argv)):
+    for x in range(len(sys.argv)):
         if "-h" in sys.argv or "--help" in sys.argv:
             help()
             sys.exit(0)
         if "-f" not in sys.argv or not is_url(sys.argv[1]):
             print("Bad input")
             sys.exit(0)
-        if sys.argv[x-1] == '-l' or sys.argv[x-1] == '-f' or sys.argv[x-1] == '-d':
+        if sys.argv[x-1] == '-l' or sys.argv[x-1] == '-f' or \
+                sys.argv[x-1] == '-d':
             continue
         if sys.argv[x] == '-l':
-            print ('search level  : ' , end = '')
-            print (sys.argv[x+1])
+            print('search level  : ', end='')
+            print(sys.argv[x+1])
             search_level = int(sys.argv[x+1])
         elif is_url(sys.argv[x]):
-            print ("search url    : " , end = '')
-            print (sys.argv[x] )
+            print("search url    : ", end='')
+            print(sys.argv[x])
             url = sys.argv[x]
         elif sys.argv[x] == '-f':
-            print ("search format : " , end = '')
-            print (sys.argv[x+1])
+            print("search format : ", end='')
+            print(sys.argv[x+1])
             file_format = sys.argv[x+1]
             if not file_format.startswith('.'):
                 file_format = '.' + file_format
         elif sys.argv[x] == '-d':
-            print ('saving into   : ' , end = '')
-            print (sys.argv[x+1])
+            print('saving into   : ', end='')
+            print(sys.argv[x+1])
             save_directory = sys.argv[x+1]
             if not os.path.exists(save_directory):
-                if input('directory does not exist create it (y/n)? : ') == 'y':
+                if input('directory does not exist create it (y/n)?: ') == 'y':
                     os.makedirs(save_directory)
                 else:
                     sys.exit(0)
@@ -151,16 +169,17 @@ if __name__ == '__main__':
     append_file_content = set()
     all_of_links = set()
 
-    for x in range(0 , search_level):
+    for x in range(search_level):
         all_of_links = all_of_links.union(file_content)
-        files_to_download = files_to_download.union(get_sutable_links(file_content , file_format))
+        files_to_download = files_to_download.union(get_sutable_links(
+                    file_content, file_format))
         for y in file_content:
-            new_links = find_links_of(y , x)
+            new_links = find_links_of(y, x)
             append_file_content = append_file_content.union(new_links)
 
         append_file_content.discard(all_of_links)
         file_content = append_file_content
         append_file_content = set()
 
-# downloading(file_content , files_to_download , save_directory)# , timeout_time)
+# downloading(file_content , files_to_download , save_directory)
     partial_download(url="http://ipv4.download.thinkbroadband.com/5MB.zip")
